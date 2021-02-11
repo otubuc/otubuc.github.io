d437929be89681b60777fdf100b7e4b93bcdd072
otubuc <otu20001@byui.edu>
2021-02-11 10:02:08 -0700

    week6

---
+++ lesson5/js/announcement.js
@@ -0,0 +1,16 @@
+window.onload = function toggleAnnouncement() {
+  // a test to see if my function is getting called when the page loads
+  //   console.log("it worked");
+
+  // a test to see if I can get the element by ID
+  //   console.log(document.getElementById("pancakes").classList);
+
+  let d = new Date();
+  let q = d.getDay();
+
+  if (q != 5) {
+    document.getElementById("pancakes").classList.toggle("hide");
+  }
+  //A test to make sure it worked
+  //   console.log(q);
+};
---
+++ lesson5/js/myelement1.js
@@ -0,0 +1,12 @@
+window.onload = function toggleAnnouncement() {
+
+  
+    let d = new Date();
+    let q = d.getDay();
+  
+    if (q==5) {
+      document.getElementById("pancakes").classList.toggle("hide");
+    }
+    //A test to make sure it worked
+    //   console.log(q);
+  };
