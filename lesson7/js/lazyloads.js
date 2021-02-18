24f34145a885a0a91c1f31d052eb47e31126cdc2
otubuc <otu20001@byui.edu>
2021-02-17 21:34:17 -0700

    lazyloads-js

--- lesson7/js/lazyloads.js
+++ lesson7/js/lazyloads.js
@@ -1,18 +1,50 @@
-let lazyImages = [...document.querySelectorAll('.lazy-image')];
-let inAdvance = 300;
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyloadImages;    
 
-function lazyLoad() {
-  lazyImages.forEach(image => {
-    if (image.offsetTop < window.innerHeight + window.pageYOffset + inAdvance) {
-      image.src = image.dataset.src;
-      image.onload = () => image.classList.add('loaded');
+  if ("IntersectionObserver" in window) {
+    lazyloadImages = document.querySelectorAll(".lazy");
+    var imageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          var image = entry.target;
+          image.src = image.dataset.src;
+          image.classList.remove("lazy");
+          imageObserver.unobserve(image);
+        }
+      });
+    });
+
+    lazyloadImages.forEach(function(image) {
+      imageObserver.observe(image);
+    });
+  } else {  
+    var lazyloadThrottleTimeout;
+    lazyloadImages = document.querySelectorAll(".lazy");
+    
+    function lazyload () {
+      if(lazyloadThrottleTimeout) {
+        clearTimeout(lazyloadThrottleTimeout);
+      }    
+
+      lazyloadThrottleTimeout = setTimeout(function() {
+        var scrollTop = window.pageYOffset;
+        lazyloadImages.forEach(function(img) {
+            if(img.offsetTop < (window.innerHeight + scrollTop)) {
+              img.src = img.dataset.src;
+              img.classList.remove('lazy');
+            }
+        });
+        if(lazyloadImages.length == 0) { 
+          document.removeEventListener("scroll", lazyload);
+          window.removeEventListener("resize", lazyload);
+          window.removeEventListener("orientationChange", lazyload);
+        }
+      }, 20);
     }
-  });
 
-  // if all loaded removeEventListener
-}
+    document.addEventListener("scroll", lazyload);
+    window.addEventListener("resize", lazyload);
+    window.addEventListener("orientationChange", lazyload);
+  }
+})
 
-lazyLoad();
-
-window.addEventListener('scroll', _.throttle(lazyLoad, 16));
-window.addEventListener('resize', _.throttle(lazyLoad, 16));
