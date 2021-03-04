6d235e9b76b98649886527af478ee99cdf1347b4
otubuc <otu20001@byui.edu>
2021-03-03 22:42:47 -0700

    changes

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -8,23 +8,21 @@ fetch(requestURL)
     console.table(jsonObject);  // temporary checking for valid response and data parsing
     const prophets = jsonObject['prophets'];
     for (let i = 0; i < prophets.length; i++ ) {
-        let card = document.createElement('section');
-        let h2 = document.createElement('h2');
-        let image =document.createElement('img');
-        let p = document.createElement('p');
+let card = document.createElement('section');
+let h2 = document.createElement('h2');
+let p = document.createElement('p');
+let image =document.createElement('img');
 
+h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+p.textContent =  'Date of Birth:' + ' ' + prophets[i].birthdate + ' ' + 'Death:' + '' + prophets[i].death + ' ' + 'Length:' + '' + prophets[i].length + ' ' +
+'Order:' + ' ' + prophets[i].order + ' ' + 'Birth place:' + ' ' + prophets[i].birthplace + ' ' + 'Children:' + ' ' + prophets[i].numofchildren;
+image.setAttribute('src', prophets[i].imageurl);
 
-        image.setAttribute('src', prophets[i].imageurl);
-        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-        p.textContent =  'Date of Birth:' + ' ' + prophets[i].birthdate + ' ' + 'Death:' + '' + prophets[i].death + ' ' + 'Length:' + '' + prophets[i].length + ' ' +
-        'Order:' + ' ' + prophets[i].order + ' ' + 'Birth place:' + ' ' + prophets[i].birthplace + ' ' + 'Children:' + ' ' + prophets[i].numofchildren;
+card.appendChild(h2);
+card.appendChild(p);
+card.appendChild(image);
 
-
-        card.appendChild(image);
-        card.appendChild(h2);
-        card.appendChild(p);
-
-
-        document.querySelector('div.cards').appendChild(card);
-    }
+document.querySelector('div.cards').appendChild(card);
+}
   });
+  
